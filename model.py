8b6fa0a116f8b9eae33eacaad25556a3dc77595b
chrisjleong <82801074+chrisjleong@users.noreply.github.com>
2021-04-18 21:49:04 -1000

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 ############################################
+# EE635 Final Project
 # Integration of V2G for Renewables
+# Collaborators: Beck, David;
+#                Leong, Christopher;
+#                Sahara, Matthew W.;
 # 
-#
 ############################################
 
 
@@ -12,3 +15,25 @@ from pyomo.environ import (
 )
 
 m = AbstractModel()
+
+#Objective: Optimize ? (cost)
+
+
+#Insert a lot of constraints
+#Curtailed energy must not exceed measured for data set
+#
+#
+#CO2 emission free
+
+#What do we need?
+#Data on V2G
+    #Cost to set up and maintain (charging station, vehicle)
+    #Power data
+#Model parameters
+#Emergency cases: as reserves, model backfeed, use of vehicles as power source
+    #cost analysis v battery degradation
+    #time trace frequency of grid
+#bulk charging?
+#quantify services and optimize cost
+#compare to grid connected default batteries already used
+#or how much of this service do we need?
