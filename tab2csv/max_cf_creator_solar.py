2f5d23e38919fe930e55148b02ec6fa37c5a55c7
saharama <saharama@hawaii.edu>
2021-05-02 22:25:37 -1000

    finished wind data, working on solar rand

---
+++ tab2csv/max_cf_creator_solar.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math
+from scipy.stats import truncnorm
+
+#input file
+fin = open("tab_loads.txt", "rt")
+#output file to write the result to
+fout = open("max_cf_solar.csv", "wt")
+
+i = 0
+#for each line in the input file
+for line in fin:
+	stuff = line.split('\t')
+	stuff.pop(0)
+	if i > 0:
+		timedate = stuff[0]
+		year = timedate[0:4] + '-'
+		mon = timedate[4:6]+'-'
+		day = timedate[6:8]
+		hour = timedate[8:10]+':00'
+		timepoint = ''.join([year, mon, day, ' ', hour])
+		date = ''.join([year,mon,day])
+
+		# define gaussian distribution for solar
+		#mu, sigma = 0.5, 0.5
+		#gaussian = np.random.normal(mu, sigma, 24)
+		# solar generation must be positive
+		#solar_hour = int(timedate[8:10])
+		
+		scipy.stats.truncnorm
+
+		# prepare data point
+		textline = ','.join([timepoint,'Solar', str(rando_load)])
+	else:
+		# initialize the first line
+		textline = ','.join([stuff[0],'date',stuff[1]])
+
+	fout.write(textline)
+	fout.write('\n')
+	i = i + 1
+
+	#read replace the string and write to output file
+
+#close input and output files
+fin.close()
+fout.close()
