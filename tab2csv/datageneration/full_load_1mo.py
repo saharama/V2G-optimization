3b905cc3ed5175af6f2d6a3683dc81a767e0325e
saharama <saharama@hawaii.edu>
2021-05-06 07:09:37 -1000

    updated full load creation code

--- tab2csv/datageneration/full_load_1mo.py
+++ tab2csv/datageneration/full_load_1mo.py
@@ -5,7 +5,7 @@ fin = open("times.csv", "rt")
 #output file to write the result to
 fout = open("loads.csv", "wt")
 
-fout.write('LOAD_ZONE,TIMEPOINT,zone_demand_mw' + '\n')
+fout.write('timepoint, date, system_load' + '\n')
 
 #for each line in the input file
 for line in fin:
@@ -22,12 +22,12 @@ for line in fin:
 	# random number/load generation
 	rng = np.random.default_rng()
 	rfloat = rng.random()
-	cos_meathead = [0.5,0.5,0.5,0.5,0.5,0.4,0.3,0.5,0.9,1,0.85,0.6]
+	cos_meathead = [0.4,0.4,0.5,0.6,0.5,0.4,0.3,0.5,0.9,1,0.85,0.6]
 	time_index = int(hour)
 	#print(rfloat)
 
 	# estimate of load characteristics based on min/max of 2020 est. load data
-	load = 550 + 700 rfloat * cos_meathead[int(time_index/2)] 
+	load = 550 + 700 * rfloat * cos_meathead[int(time_index/2)] 
 
 	datepoint_NBC = year + '-' + month + '-' + day
 	textline = ','.join(['Oahu',timepoint[:-1],datepoint_NBC,str(load)])
