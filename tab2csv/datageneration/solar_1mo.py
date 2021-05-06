1a2629d07b092069dd217c93e8adb44ad9158484
saharama <saharama@hawaii.edu>
2021-05-06 06:53:07 -1000

    produced solar generation

--- tab2csv/datageneration/solar_1mo.py
+++ tab2csv/datageneration/solar_1mo.py
@@ -1,56 +1,50 @@
 import numpy as np
-import math
-from scipy.stats import truncnorm
+# import math
+# from scipy.stats import truncnorm
 
 #input file
-fin = open("tab_loads.txt", "rt")
+fin = open("times.csv", "rt")
 #output file to write the result to
-fout = open("max_cf_solar.csv", "wt")
+fout = open("month_max_cf_solar.csv", "wt")
 
 i = 0
 #for each line in the input file
 for line in fin:
-	stuff = line.split('\t')
-	stuff.pop(0)
-	if i > 0:
-		timedate = stuff[0]
-		year = timedate[0:4] + '-'
-		mon = timedate[4:6]+'-'
-		day = timedate[6:8]
-		hour = timedate[8:10]+':00'
-		timepoint = ''.join([year, mon, day, ' ', hour])
-		date = ''.join([year,mon,day])
 
-		# define gaussian distribution for solar
-		#mu, sigma = 0.5, 0.5
-		#gaussian = np.random.normal(mu, sigma, 24)
-		# solar generation must be positive
-		#solar_hour = int(timedate[8:10])
-		
-		# scipy.stats.truncnorm
-		
-		# random number/load generation
-#		rng = np.random.default_rng()
-#		rfloat = rng.random()
-#		cos_meathead = [0,0,0,0.5,0.85,0.97,1,0.97,0.85,0.5,0,0]
-#		time_index = int(timedate[8:10])
-#		print(rfloat)
-#		if time_index > 18 or time_index < 6:
-#			solar_gen = 0
-#		else:
-#			solar_gen = rfloat * cos_meathead[int(time_index/2)] 
+# read in data like a meathead
+	timepoint = line
+	year = line[0:4]
+	month = line[5:7]
+	day = line[8:10]
+	hour = line[11:13]
+	minute = line[14:16] 
+	# debug:
+	#print(year + month + day + ' ' + hour +':' + minute)
 
-		# prepare data point
-		textline = ','.join([timepoint,'Solar', str(solar_gen)])
+	# define gaussian distribution for solar
+	#mu, sigma = 0.5, 0.5
+	#gaussian = np.random.normal(mu, sigma, 24)
+	# solar generation must be positive
+	#solar_hour = int(timedate[8:10])
+		
+	# scipy.stats.truncnorm
+		
+	# random number/load generation
+	rng = np.random.default_rng()
+	rfloat = rng.random()
+	cos_meathead = [0,0,0,0.5,0.85,0.97,1,0.97,0.85,0.5,0,0]
+	time_index = int(hour)
+	#print(rfloat)
+	if time_index > 18 or time_index < 6:
+		solar_gen = 0
 	else:
-		# initialize the first line
-		textline = ','.join([stuff[0],'date',stuff[1]])
+		solar_gen = rfloat * cos_meathead[int(time_index/2)] 
+
+	# prepare data point
+	textline = ','.join([timepoint[:-1],'Solar', str(solar_gen)])
 
 	fout.write(textline)
 	fout.write('\n')
-	i = i + 1
-
-	#read replace the string and write to output file
 
 #output files
 fin.close()
